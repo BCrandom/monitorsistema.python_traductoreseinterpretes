213d263a62bbfe69c1c9da551d905a7384783125
BCrandom <coreabarbara306@gmail.com>
2026-01-29 22:04:08 -0400

    Cambios esteticos y adicion de nombre al sistema

--- src/index.py
+++ src/index.py
@@ -7,18 +7,38 @@ import Alpha_0_1
 def limpiar_pantalla():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+banner = """
+┌─── [ GROUP_#01 ] ────────────────────────────────────────┐
+│                                                          │
+│   ﹃                                                 ﹄  │
+│      _____                      _____                    │
+│     / ___/____  ____ _____     / ___/__  _______         │
+│     \__ \/ __ \/ __ `/ __ \    \__ \/ / / / ___/         │
+│    ___/ / / / / /_/ / /_/ /   ___/ / /_/ (__  )          │
+│   /____/_/ /_/\__,_/ .___/   /____/\__, /____/           │
+│                   /_/             /____/                 │
+│                                                          │
+│   ﹄                                                 ﹃  │
+│             Te-I  [•]  S01  L  3:2  100% [===]           │
+└──────────────────────────────────────────────────────────┘
+"""
+
 def mostrar_menu():
     limpiar_pantalla()
-    print("==========================================")
-    print("   SISTEMA DE MONITOREO PROFESIONAL V1.0")
-    print("==========================================")
+    print(" ▥  " + "|  |  " * 10)
+    #print("==========================================")
+    print(banner)
+    print("   SISTEMA DE MONITOREO PROFESIONAL V1.0\n")
+    print(" ▥  " + "|  |  " * 10 + "\n")
+    #print("==========================================")
     print("1. [CORE] Monitoreo en Tiempo Real (CPU/RAM/Disco)")
     print("2. [CORE] Ver Top 5 Procesos (Consumo Crítico)")
     print("3. [ANALISIS] Snapshot del Sistema (Uptime/Hilos)")
     print("4. [DATOS] Configurar Guardado Automático (CSV)")
     print("5. [REPORTE] Generar Informe PDF de Anomalías")
     print("6. Salir")
-    print("==========================================")
+    print(" ₊˚.༄  " + "˚‧⁺  ･ ˖ ·" * 5)
+    #print("==========================================")
         
 def menu_principal():
     while True:
